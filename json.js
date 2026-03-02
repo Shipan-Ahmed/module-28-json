866b17d1d406b59ff173c9f1676a911fecbc1465
Shipan Ahmed <shipan.cse.brur@gmail.com>
2026-03-02 15:05:42 +0600

    first commit

---
+++ json.js
@@ -0,0 +1,19 @@
+jsonjson =  {
+        name: "json",
+        description: "A JSON parser and serializer.",
+        age: 10,
+        website: "https://www.json.org/",
+        license: "Various (e.g., MIT, Apache 2.0, etc.)",
+        languages: ["JavaScript", "Python", "Java", "C#", "Go", "Ruby", "PHP", "C++", "Rust", "Swift"],
+}
+
+console.log(jsonjson);
+console.log(typeof jsonjson);
+
+const josonStrding = JSON.stringify(jsonjson);
+console.log(josonStrding);
+console.log(typeof josonStrding);
+
+const josonParse = JSON.parse(josonStrding);
+console.log(josonParse);
+console.log(typeof josonParse);
